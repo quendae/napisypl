fe304f9fdccca864b99715dd62aa94ff70a80842
quendae <qnd.000@gmail.com>
2026-09-12 14:41:10 +0200

    test: require eligible addressee gender evidence

--- tests/NapisyPL.Core.Tests/SurgicalGenderContextGuardTests.cs
+++ tests/NapisyPL.Core.Tests/SurgicalGenderContextGuardTests.cs
@@ -5,7 +5,7 @@ namespace NapisyPL.Core.Tests;
 public sealed class SurgicalGenderContextGuardTests
 {
     [Fact]
-    public void CanApply_SecondPersonEditRequiresKnownAddressee()
+    public void CanApply_SecondPersonEditRequiresKnownAddresseeAndEligibleEvidence()
     {
         var edit = new SurgicalGenderEdit(
             50,
@@ -15,7 +15,12 @@ public sealed class SurgicalGenderContextGuardTests
             GenderAgreementTarget.Addressee);
 
         Assert.False(SurgicalGenderContextGuard.CanApply(edit, "SPEAKER_A", probableAddressee: null));
-        Assert.True(SurgicalGenderContextGuard.CanApply(edit, "SPEAKER_A", "SPEAKER_B"));
+        Assert.False(SurgicalGenderContextGuard.CanApply(edit, "SPEAKER_A", "SPEAKER_B"));
+        Assert.True(SurgicalGenderContextGuard.CanApply(
+            edit,
+            "SPEAKER_A",
+            "SPEAKER_B",
+            probableAddresseeGenderEvidence: new SpeakerGenderEvidence(SpeakerVoiceGender.Female, 0.96, 2)));
     }
 
     [Fact]
@@ -52,6 +57,46 @@ public sealed class SurgicalGenderContextGuardTests
             currentSpeakerGenderEvidence: new SpeakerGenderEvidence(SpeakerVoiceGender.Female, 0.95, 2)));
     }
 
+    [Fact]
+    public void CanApply_AddresseeDirectionMustMatchResolvedAddresseeGender()
+    {
+        var toFemale = new SurgicalGenderEdit(
+            50,
+            "zrobiłeś",
+            "zrobiłaś",
+            0.99,
+            GenderAgreementTarget.Addressee);
+        var toMale = new SurgicalGenderEdit(
+            50,
+            "zrobiłaś",
+            "zrobiłeś",
+            0.99,
+            GenderAgreementTarget.Addressee);
+        var female = new SpeakerGenderEvidence(SpeakerVoiceGender.Female, 0.97, 3);
+        var male = new SpeakerGenderEvidence(SpeakerVoiceGender.Male, 0.97, 3);
+
+        Assert.True(SurgicalGenderContextGuard.CanApply(
+            toFemale,
+            "SPEAKER_A",
+            "SPEAKER_B",
+            probableAddresseeGenderEvidence: female));
+        Assert.False(SurgicalGenderContextGuard.CanApply(
+            toFemale,
+            "SPEAKER_A",
+            "SPEAKER_B",
+            probableAddresseeGenderEvidence: male));
+        Assert.True(SurgicalGenderContextGuard.CanApply(
+            toMale,
+            "SPEAKER_A",
+            "SPEAKER_B",
+            probableAddresseeGenderEvidence: male));
+        Assert.False(SurgicalGenderContextGuard.CanApply(
+            toMale,
+            "SPEAKER_A",
+            "SPEAKER_B",
+            probableAddresseeGenderEvidence: female));
+    }
+
     [Theory]
     [InlineData(GenderAgreementTarget.Speaker)]
     [InlineData(GenderAgreementTarget.Addressee)]
@@ -68,6 +113,7 @@ public sealed class SurgicalGenderContextGuardTests
             edit,
             "SPEAKER_A",
             "SPEAKER_B",
+            new SpeakerGenderEvidence(SpeakerVoiceGender.Female, 0.99, 2),
             new SpeakerGenderEvidence(SpeakerVoiceGender.Female, 0.99, 2)));
     }
 }
