f36fd895def13e3bf7e190a52e27c689ca684c3c
quendae <qnd.000@gmail.com>
2026-09-14 10:40:56 +0200

    cleanup: remove obsolete local LLM reviewer stack

--- src/NapisyPL.Core/ContextResolution/IContextResolver.cs
+++
@@ -1,8 +0,0 @@
-namespace NapisyPL.Core.ContextResolution;
-
-public interface IContextResolver
-{
-    Task<ContextMap> ResolveAsync(
-        IReadOnlyList<ContextCue> cues,
-        CancellationToken cancellationToken = default);
-}
