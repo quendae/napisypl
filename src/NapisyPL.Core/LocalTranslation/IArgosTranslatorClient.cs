b36ba4af80744b74fadf65cca3680f63ffb9e7e1
quendae <qnd.000@gmail.com>
2026-09-14 10:33:13 +0200

    cleanup: remove retired local translation stacks

--- src/NapisyPL.Core/LocalTranslation/IArgosTranslatorClient.cs
+++
@@ -1,8 +0,0 @@
-namespace NapisyPL.Core.LocalTranslation;
-
-public interface IArgosTranslatorClient
-{
-    Task<IReadOnlyList<string>> TranslateAsync(
-        IReadOnlyList<string> texts,
-        CancellationToken cancellationToken = default);
-}
